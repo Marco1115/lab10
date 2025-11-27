b5447837b21bd62361678895ccd65de4093f19f3
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-11-27 19:08:22 +0100

    Corrected checkstyle error in DrawNumberImpl

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/Configuration.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/Configuration.java
@@ -1,6 +1,5 @@
 package it.unibo.mvc;
 
-
 /**
  * Encapsulates the concept of configuration.
  */
@@ -46,16 +45,13 @@ public final class Configuration {
 
     /**
      * Pattern builder: used here because:
-     * 
      * - all the parameters of the Configuration class have a default value, which
      * means that we would like to have all the possible combinations of
      * constructors (one with three parameters, three with two parameters, three
      * with a single parameter), which are way too many and confusing to use
-     * 
      * - moreover, it would be impossible to provide all of them, because they are
      * all of the same type, and only a single constructor can exist with a given
      * list of parameter types.
-     * 
      * - the Configuration class has three parameters of the same type, and it is
      * unclear to understand, in a call to its contructor, which is which. By using
      * the builder, we emulate the so-called "named arguments".
@@ -73,29 +69,29 @@ public final class Configuration {
         private boolean consumed = false;
 
         /**
-         * @param min the minimum value
+         * @param minimum the minimum value
          * @return this builder, for method chaining
          */
-        public Builder setMin(final int min) {
-            this.min = min;
+        public Builder setMin(final int minimum) {
+            this.min = minimum;
             return this;
         }
 
         /**
-         * @param max the maximum value
+         * @param maximum the maximum value
          * @return this builder, for method chaining
          */
-        public Builder setMax(final int max) {
-            this.max = max;
+        public Builder setMax(final int maximum) {
+            this.max = maximum;
             return this;
         }
 
         /**
-         * @param attempts the attempts count
+         * @param attemptsNumber the attempts count
          * @return this builder, for method chaining
          */
-        public Builder setAttempts(final int attempts) {
-            this.attempts = attempts;
+        public Builder setAttempts(final int attemptsNumber) {
+            this.attempts = attemptsNumber;
             return this;
         }
 
